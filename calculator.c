0daa3b5b334d241f64e46fdfd2becfa8f5236530
takkw504 <takloon@gmail.com>
2017-11-15 18:16:22 +0100

    uppdaterad calculator.c

--- calculator.c
+++ calculator.c
@@ -6,31 +6,40 @@
 #include "subtraction.h"
 
 int main(void){
-	
+
 	system("CHCP 1252");
     system("CLS");
-	
-	float a,b;
-	
+
+	float a, b;
+
 	char choice, x;
-	
+
 	while(1){
-		
-		printf("meny!");
+
+		printf("Kalkylator!\nVälj mellan A/M/D/S\nA. Addition\nM. Multiplikation\nD. Division\nS. Subtraktion\n");
 		scanf("%c", &choice);
-		
+
 		char throw_away = getchar();
-		
+
 		switch(choice){
 			case 'A': case 'a':{
-				
-				printf("Vill du forsätta?");
-				scanf("%c", &x);
-				
+				system("CLS");
+				printf("\nAnge tal 1 du vill addera: ");
+				scanf("%f", &a);
 				getchar();
-				
+				printf("\nAnge tal 2 du vill addera: ");
+				scanf("%f", &b);
+				getchar();
+
+				printf("\nSvar: %lf", add(a,b));
+
+				printf("\nVill du forsätta?");
+				scanf("%c", &x);
+
+				getchar();
+
 				if(x == 'j' || 'J'){
-					printf("här!");
+					system("CLS");
 					continue;
 				}
 				else{
@@ -39,24 +48,92 @@ int main(void){
 				continue;
 			}
 			case 'M': case 'm':{
-				printf("dsfsdafdsa");
+				system("CLS");
+				printf("\nAnge tal 1 du vill multiplicera: ");
+				scanf("%f", &a);
+				getchar();
+				printf("\nAnge tal 2 du vill multiplicera: ");
+				scanf("%f", &b);
+				getchar();
+
+				printf("\nSvar: %lf", mult(a,b));
+
+				printf("\nVill du forsätta?");
+				scanf("%c", &x);
+
+				getchar();
+
+				if(x == 'j' || 'J'){
+					system("CLS");
+					continue;
+				}
+				else{
+					exit(1);
+				}
 				continue;
 			}
 			case 'D': case 'd':{
+				system("CLS");
+				printf("\nAnge tal 1 du vill dividera: ");
+				scanf("%f", &a);
+				getchar();
+				printf("\nAnge tal 2 du vill dividera: ");
+				scanf("%f", &b);
+				getchar();
+
+				printf("\nSvar: %lf", a/b);
+
+				printf("\nVill du forsätta?");
+				scanf("%c", &x);
+
+				getchar();
+
+				if(x == 'j' || 'J'){
+					system("CLS");
+					continue;
+				}
+				else{
+					exit(1);
+				}
 				continue;
 			}
 			case 'S': case 's':{
+				system("CLS");
+				printf("\nAnge tal 1 du vill subtrahera: ");
+				scanf("%f", &a);
+				getchar();
+				printf("\nAnge tal 2 du vill subtrahera: ");
+				scanf("%f", &b);
+				getchar();
+
+				printf("\nSvar: %lf", sub(a,b));
+
+				printf("\nVill du forsätta?");
+				scanf("%c", &x);
+
+				getchar();
+
+				if(x == 'j' || 'J'){
+					system("CLS");
+					continue;
+				}
+				else{
+					exit(1);
+				}
 				continue;
 			}
 			case 'E': case 'e':{
 				exit(1);
 			}
 			default:{
+				printf("Här kan du endast svara M/D/S/A, försök igen!");
+				printf("pause");
+				system("CLS");
 				continue;
 			}
 		}
 	break;
 	}
-	
+
 	return 0;
 }
